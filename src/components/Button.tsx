faebb7fcb56142f236a3f596652ca440c630af21
= <hotalog@hota1024.com>
2022-02-06 15:35:37 +0900

    💄remove text-decoration from Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,6 +14,7 @@ export const Button = styled('button', {
   color: '#202020',
   cursor: 'pointer',
   transition: 'all 100ms ease',
+  textDecoration: 'none',
   variants: {
     variant: {
       default: {

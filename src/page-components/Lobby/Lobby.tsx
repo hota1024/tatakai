b5015db8c6ba645da9853c2b7bcc3561fce50ec8
= <hotalog@hota1024.com>
2022-02-06 16:47:37 +0900

    💄wrap lobby with centered

--- src/page-components/Lobby/Lobby.tsx
+++ src/page-components/Lobby/Lobby.tsx
@@ -9,6 +9,7 @@ import { ExitAnimationable } from '@/components/ExitAnimationable'
 import { FullHeightContainer } from '@/components/FullHeightContainer'
 import { upFadeIn } from '@/utils/transition'
 import { motion } from 'framer-motion'
+import { Centered } from '@/components/Centered'
 
 /**
  * Lobby props.
@@ -22,30 +23,32 @@ export const Lobby: React.VFC<LobbyProps> = (props) => {
   return (
     <ExitAnimationable>
       <FullHeightContainer>
-        <motion.h1 variants={upFadeIn}>ロビー</motion.h1>
-        <Box css={{ margin: '16px 0' }} />
-        <Box css={{ display: 'flex' }} variants={upFadeIn}>
-          <Link href="/rooms/new" passHref>
-            <AreaButton as="a" css={{ margin: '4px' }}>
-              <AreaButtonLabel>➕ 部屋を作る</AreaButtonLabel>
-              <AreaButtonContent>
-                お題を選んで
-                <br />
-                対戦相手を待とう
-              </AreaButtonContent>
-            </AreaButton>
-          </Link>
-          <Link href="/rooms/join" passHref>
-            <AreaButton as="a" css={{ margin: '4px' }}>
-              <AreaButtonLabel>⏩ 部屋に参加する</AreaButtonLabel>
-              <AreaButtonContent>
-                招待を受け取ったら
-                <br />
-                ここから参加してね
-              </AreaButtonContent>
-            </AreaButton>
-          </Link>
-        </Box>
+        <Centered variants={upFadeIn}>
+          <motion.h1>ロビー</motion.h1>
+          <Box css={{ margin: '16px 0' }} />
+          <Box css={{ display: 'flex' }}>
+            <Link href="/rooms/new" passHref>
+              <AreaButton as="a" css={{ margin: '4px' }}>
+                <AreaButtonLabel>➕ 部屋を作る</AreaButtonLabel>
+                <AreaButtonContent>
+                  お題を選んで
+                  <br />
+                  対戦相手を待とう
+                </AreaButtonContent>
+              </AreaButton>
+            </Link>
+            <Link href="/rooms/join" passHref>
+              <AreaButton as="a" css={{ margin: '4px' }}>
+                <AreaButtonLabel>⏩ 部屋に参加する</AreaButtonLabel>
+                <AreaButtonContent>
+                  招待を受け取ったら
+                  <br />
+                  ここから参加してね
+                </AreaButtonContent>
+              </AreaButton>
+            </Link>
+          </Box>
+        </Centered>
       </FullHeightContainer>
     </ExitAnimationable>
   )

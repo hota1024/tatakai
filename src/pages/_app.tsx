9750a9a5299a5dad85809c37b7692c42cd8ec426
= <hotalog@hota1024.com>
2022-02-06 14:16:28 +0900

    💄add noto sans jp font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
+import { globalCss } from '@/stitches.config'
 import type { AppProps } from 'next/app'
 
+const globalStyles = globalCss({
+  body: {
+    fontFamily: 'Noto Sans JP',
+  },
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
+
   return (
     <>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,10 @@ export default class Document extends NextDocument {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;500;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />

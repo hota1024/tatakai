d1f285fb29ce2a59fe10c65a8773e5300e618efa
= <hotalog@hota1024.com>
2022-02-06 15:11:16 +0900

    💄add FullHeightContainer component

---
+++ src/components/FullHeightContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@/stitches.config'
+
+export const FullHeightContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})

5f6589926bb49b58e31c521a6a8f1be9c5ed21b1
= <hotalog@hota1024.com>
2022-02-06 16:35:11 +0900

    💄add upFadeIn transition

--- src/utils/transition.ts
+++ src/utils/transition.ts
@@ -1,7 +1,23 @@
+import { Variants } from 'framer-motion'
+
 export const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] }
 
 export const fadeIn = {
   initial: { opacity: 0 },
   enter: { opacity: 1 },
   exit: { opacity: 0 },
+}
+
+export const upFadeIn: Variants = {
+  initial: { y: '16px', opacity: 0, transition },
+  enter: {
+    y: '0',
+    opacity: 1,
+    transition,
+  },
+  exit: {
+    y: '16px',
+    opacity: 0,
+    transition,
+  },
 }

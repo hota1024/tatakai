1b53c135e785045b524a57328d43d612a9c1ff6e
= <hotalog@hota1024.com>
2022-02-06 13:24:53 +0900

    🎉init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - ai-library</title>
+      </Head>
+
+      <div>ai-library</div>
+    </>
+  )
+}
+
+export default HomePage

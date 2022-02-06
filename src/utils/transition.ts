b280c4faa402d439d170be18790f528ce55ffc4c
= <hotalog@hota1024.com>
2022-02-06 15:55:11 +0900

    💄add transition and fadeIn variant

---
+++ src/utils/transition.ts
@@ -0,0 +1,7 @@
+export const transition = { duration: 0.5, ease: [0.43, 0.13, 0.23, 0.96] }
+
+export const fadeIn = {
+  initial: { opacity: 0 },
+  enter: { opacity: 1 },
+  exit: { opacity: 0 },
+}

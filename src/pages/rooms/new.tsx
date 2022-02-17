7bfbc32e316a0ba742772f54ff32ba064a387194
= <hotalog@hota1024.com>
2022-02-17 17:26:59 +0900

    ✨add RoomsNew page

---
+++ src/pages/rooms/new.tsx
@@ -0,0 +1,11 @@
+import { NewRoom } from '@/page-components/NewRoom/NewRoom'
+import { NextPage } from 'next'
+
+/**
+ * NewRoom page.
+ */
+export const NewRoomPage: NextPage = () => {
+  return <NewRoom />
+}
+
+export default NewRoomPage

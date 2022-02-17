f933e7f6f7d4d40994dcd753709b2c219f60bee7
= <hotalog@hota1024.com>
2022-02-17 18:12:53 +0900

    ✨add user registeration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { useInterval } from '@/hooks/useInterval'
 import { globalCss } from '@/stitches.config'
+import { User } from '@/types/User'
+import axios from 'axios'
 import { AnimatePresence, motion } from 'framer-motion'
+import { useAtom } from 'jotai'
+import { SuccessApiResponse } from 'next-api-handler'
 import type { AppProps } from 'next/app'
+import { useCallback, useEffect } from 'react'
+import { userAtom } from './atoms/user'
 
 const globalStyles = globalCss({
   body: {
@@ -14,8 +21,30 @@ const globalStyles = globalCss({
   },
 })
 
-function MyApp({ Component, pageProps, router }: AppProps) {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
   globalStyles()
+
+  const [user, setUser] = useAtom(userAtom)
+
+  const createUser = useCallback(async () => {
+    const { data } = await axios.post<SuccessApiResponse<User>>('/api/users')
+
+    setUser(data.data)
+  }, [setUser])
+
+  useEffect(() => {
+    if (!user) {
+      createUser()
+    }
+  }, [createUser, user])
+
+  useInterval(() => {
+    if (!user) return
+
+    axios.post(`/api/users/${user.id}/heartbeat`).catch(() => {
+      createUser()
+    })
+  }, 3000)
 
   return (
     <>
@@ -25,5 +54,3 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     </>
   )
 }
-
-export default MyApp
